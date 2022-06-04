ec6ca439a89d759139c779b6ed48046baac0796a
Derine Banis <d.banis.13@gmail.com>
2022-06-03 18:16:21 -0700

    Recursion practice with multi-level arrays.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let total = 0;
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      element = sumItems(element);
+    }
+    total += element;
+  }
 
+  return total;
+};
 module.exports = sumItems;
